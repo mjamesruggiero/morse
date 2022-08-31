1c80a7ff26a6f66b1625493a36e4ce3d72c697b2
mjamesruggiero <mjamesruggiero@gmail.com>
2022-08-31 13:47:54 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# morse
+
+Learning [Architecture Patterns with Python, Percival & Gregory](https://www.oreilly.com/library/view/architecture-patterns-with/9781492052197/)
+
+# what's with the name?
+
+Named after [Inspector Morse](https://en.wikipedia.org/wiki/Inspector_Morse) for no particular reason.
+
+<img src="docs/morse.gif" alt="Endeavor Morse" />
+
---
+++ test_allocate.py
@@ -0,0 +1,17 @@
+from datetime import date, timedelta
+import pytest
+from model import allocate, OrderLine, Batch, OutOfStock
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=10)
+
+def test_prefers_current_stock_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    allocate(line, [in_stock_batch, shipment_batch])
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
